227b27e8f42a16d2319438c2ecde6ecd813d1bdf
zachspang <spangzach@gmail.com>
2025-10-04 14:58:35 -0700

    Add SDL2

--- chip8-emulator.cpp
+++ chip8-emulator.cpp
@@ -2,11 +2,15 @@
 //
 
 #include "chip8-emulator.h"
+#include <SDL.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+	SDL_Init(SDL_INIT_VIDEO);
 	cout << "Hello CMake." << endl;
+
+	SDL_Quit();
 	return 0;
 }
