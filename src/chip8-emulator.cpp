e558aa24c141c5a15a94f768d38fa3400bf7e60e
zachspang <spangzach@gmail.com>
2025-10-06 17:40:14 -0700

    Add private variables for cpu class

---
+++ src/chip8-emulator.cpp
@@ -0,0 +1,26 @@
+﻿// chip8-emulator.cpp : Defines the entry point for the application.
+//
+
+#include "cpu.h"
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{ 
+	//initialize SDL graphics and input
+
+	//initialize cpu
+
+	//load game
+
+	for (;;) {
+		//emulate cycle
+
+		//update screen
+
+		//store key states in memory
+
+		break;
+	}
+
+	return 0;
+}
---
+++ src/cpu.h
@@ -0,0 +1,36 @@
+class cpu {
+public:
+	void init();
+
+	void emulateCycle();
+
+private:
+	//4kB Memory
+	unsigned char memory[4096];
+
+	//16 one byte general purpose registers, V0 - VF
+	unsigned char V[16];
+
+	//2 byte Program Counter, points to current instruction in memory
+	unsigned short PC;
+
+	//2 byte Index register points to a location in memory
+	unsigned short I;
+
+	//Stack for return addresses
+	unsigned short stack[12];
+
+	//Stack pointer
+	unsigned short sp;
+
+	//Two timer registers that decrement 60 times a second until hitting 0. sound_timer beeps when not 0
+	unsigned char delay_timer;
+	unsigned char sound_timer;
+
+	//64 * 32 grid of pixels, each is 1 or 0 because the only colors are black and white
+	unsigned char display[2048];
+
+	//States of the 16 keys on the keypad
+	unsigned char keys[16];
+
+};
